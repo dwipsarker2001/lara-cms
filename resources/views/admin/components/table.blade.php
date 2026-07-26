debf0451f45b0544f93f5458a73627880edb456a
Dwip Sarker <email.dwip@gmail.com>
2026-07-26 23:41:42 +0600

    style: add custom sleek scrollbar UI to datatables

--- resources/views/admin/components/table.blade.php
+++ resources/views/admin/components/table.blade.php
@@ -5,7 +5,7 @@
     'emptySubtext' => 'Submissions will appear here once the form is submitted.',
 ])
 
-<div class="overflow-x-auto rounded-xl ring-1 ring-content-border bg-content-bg shadow-sm">
+<div class="overflow-x-auto table-scrollbar rounded-xl ring-1 ring-content-border bg-content-bg shadow-sm">
     <table class="w-full min-w-full border-separate border-spacing-y-0 text-left text-[13px]">
         <thead>
             @if (isset($thead))
