a46c43c646fc4a8bd166af5746b9a8d5662ef643
Dwip Sarker <email.dwip@gmail.com>
2026-08-14 12:22:38 +0600

    fix(layout): add x-cloak to avatar UserMenu dropdown to eliminate flash on reload

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -156,6 +156,7 @@
 
                 <div
                     x-show="open"
+                    x-cloak
                     x-transition:enter="transition ease-out duration-100"
                     x-transition:enter-start="opacity-0 scale-95"
                     x-transition:enter-end="opacity-100 scale-100"
