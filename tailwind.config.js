813d366f659a1fc41ac7f4e6fc97f43b1c814a0c
Dwip Sarker <email.dwip@gmail.com>
2026-08-19 00:08:43 +0600

    fix: add plugins path to tailwind content scanning, scope editor colors, and bump v1.6.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
+        "./plugins/**/*.blade.php",
+        "./plugins/**/*.php",
+        "./app/Blocks/**/*.php",
     ],
 
     theme: {
